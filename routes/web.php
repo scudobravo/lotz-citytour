65a54917a1c99c8c48b7ce81e8a4cf6a7790971c
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-12 00:37:30 -0400

    New interaction point of interest

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\MonumentController;
 use App\Http\Controllers\PointController;
+use App\Http\Controllers\TwilioController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/points', [MonumentController::class, 'index']);
 
 Route::get('/point/{id}', [MonumentController::class, 'show'])->name('point.show');
+
+// Webhook per i messaggi Twilio
+Route::post('/twilio/webhook', [TwilioController::class, 'handleIncomingMessage']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
